64ec44f2b89a1e570a8148fdff3845d3cca31644
Fritz Zaucker <fritz.zaucker@oetiker.ch>
2021-01-23 21:55:38 +0100

    Add none-rich example

--- source/class/qxl/demobrowser/demo/virtual/SelectBox.js
+++ source/class/qxl/demobrowser/demo/virtual/SelectBox.js
@@ -47,6 +47,9 @@ qx.Class.define("qxl.demobrowser.demo.virtual.SelectBox",
       var advanced = new qx.ui.container.Composite(new qx.ui.layout.HBox(50));
       advanced.setPadding(20);
 
+      var incrementalSearch = new qx.ui.container.Composite(new qx.ui.layout.HBox(50));
+      incrementalSearch.setPadding(20);
+
       standard.add(this.createBox1());
       standard.add(this.createBox2());
       standard.add(this.createBox3());
@@ -55,10 +58,13 @@ qx.Class.define("qxl.demobrowser.demo.virtual.SelectBox",
       advanced.add(this.createBox5());
       advanced.add(this.createBox6());
       advanced.add(this.createBox7());
-      advanced.add(this.createBox8());
+
+      search.add(this.createBox8());
+      search.add(this.createBox9());
 
       container.add(standard, {left : 20, top : 20});
       container.add(advanced, {left : 20, top : 200});
+      container.add(search,   {left : 20, top : 380});
       this.getRoot().add(scroller, {edge : 0});
     },
 
@@ -279,6 +285,7 @@ qx.Class.define("qxl.demobrowser.demo.virtual.SelectBox",
       return container;
     },
 
+
     createBox8 : function()
     {
       var container = new qx.ui.container.Composite(new qx.ui.layout.VBox(2));
@@ -294,6 +301,29 @@ qx.Class.define("qxl.demobrowser.demo.virtual.SelectBox",
       // Creates the select box
       var selectBox = new qx.ui.form.VirtualSelectBox(model);
       selectBox.setIncrementalSearch(true);
+      container.add(selectBox);
+
+      return container;
+    },
+
+
+    createBox9 : function()
+    {
+      var container = new qx.ui.container.Composite(new qx.ui.layout.VBox(2));
+      container.add(new qx.ui.basic.Label("Incremental Search, rich labels"));
+
+      // Creates the model data
+      var rawData = [
+        'Elm street', 'Abbey road', 'Route 61', 'Central station', 'Central Park',
+        'Park Avenue', 'Broadway', 'Hohle Gasse', 'Überlandstrasse'
+      ];
+      var model   = qx.data.marshal.Json.createModel(rawData);
+
+      // Creates the select box
+      var selectBox = new qx.ui.form.VirtualSelectBox(model);
+      selectBox.setIncrementalSearch(true);
+      // Set labels and highlighting rich (HTML formatting)
+      selectBox.setRich(true);
       container.add(selectBox);
 
       return container;
