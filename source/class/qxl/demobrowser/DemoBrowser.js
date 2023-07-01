dadb4419c43a0f0b3624f3f303cc902c160fdf9d
goldim <goldim1993@gmail.com>
2023-07-02 01:18:29 +0300

    Exclude app theme in list of theme button

--- source/class/qxl/demobrowser/DemoBrowser.js
+++ source/class/qxl/demobrowser/DemoBrowser.js
@@ -512,11 +512,13 @@ qx.Class.define("qxl.demobrowser.DemoBrowser", {
         var group = new qx.ui.form.RadioGroup();
 
         var themes = qx.Theme.getAll();
-        var currentThemeItem;
         var currentTheme = qx.theme.manager.Meta.getInstance().getTheme();
         for (var key in themes) {
           let theme = themes[key];
           if (theme.type === "meta") {
+            if (theme.name === currentTheme.name){
+              continue;
+            }
             var item = new qx.ui.menu.RadioButton(theme.title);
             item.setUserData("value", theme.name);
             themeMenu.add(item);
